36f3913c6356f10a9a73d018f6b3709f272e64cd
sharibanis <41633703+sharibanis@users.noreply.github.com>
2021-05-05 08:50:19 +0800

    logs

--- src/main/java/com/sharib/spring/files/csv/controller/CSVController.java
+++ src/main/java/com/sharib/spring/files/csv/controller/CSVController.java
@@ -34,25 +34,30 @@ public class CSVController {
   public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
     String message = "";
 
+    System.out.println("Received file: "+ file.getOriginalFilename());
     if (CSVHelper.hasCSVFormat(file)) {
       try {
+   	    System.out.println("Uploading CSV file: "+ file.getOriginalFilename());
         fileService.save(file);
-
         message = "Uploaded the file successfully: " + file.getOriginalFilename();
+   	    System.out.println("Uploaded CSV file successfully: "+ file.getOriginalFilename());
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
       } catch (Exception e) {
+   	    System.out.println("Could not upload the CSV file: "+ file.getOriginalFilename());
         message = "Could not upload the file: " + file.getOriginalFilename() + "!";
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
       }
+    } else {
+        System.out.println("Received file not CSV file: "+ file.getOriginalFilename());
+        message = "Please upload a csv file!";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
     }
-
-    message = "Please upload a csv file!";
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
   }
 
-  @GetMapping("/tutorials")
+  @GetMapping("/files")
   public ResponseEntity<List<Tutorial>> getAllFiles() {
     try {
+       System.out.println("Getting all files...");
       List<Tutorial> tutorials = fileService.getAllTutorials();
 
       if (tutorials.isEmpty()) {
@@ -64,16 +69,4 @@ public class CSVController {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-
-  @GetMapping("/download")
-  public ResponseEntity<Resource> getFile() {
-    String filename = "data.csv";
-    InputStreamResource file = new InputStreamResource(fileService.load());
-
-    return ResponseEntity.ok()
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-        .contentType(MediaType.parseMediaType("application/csv"))
-        .body(file);
-  }
-
 }
